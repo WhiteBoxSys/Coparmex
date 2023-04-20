95a736fcc4418f44c32812e37d839c06c277f0ce
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2023-04-20 14:53:05 -0600

    subiendo cambios

--- components/FactsStats.php
+++ components/FactsStats.php
@@ -3,34 +3,25 @@
         <div class="row gx-0">
             <div class="col-lg-4 wow zoomIn" data-wow-delay="0.1s">
                 <div class="bg-dark shadow d-flex align-items-center justify-content-center p-4" style="height: 150px;">
-                    <div class="bg-white d-flex align-items-center justify-content-center rounded mb-2" style="width: 60px; height: 60px;">
-                        <i class="fa fa-users text-dark"></i>
-                    </div>
+                    <h1 class="text-white mb-0" data-toggle="counter-up">180</h1>
                     <div class="ps-4">
-                        <h1 class="text-white mb-0" data-toggle="counter-up">180</h1>
-                        <h5 class="text-white mb-0">Empresas</h5>
+                        <h3 class="text-white mb-0">Empresas</h3>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 wow zoomIn" data-wow-delay="0.3s">
                 <div class="bg-light shadow d-flex align-items-center justify-content-center p-4" style="height: 150px;">
-                    <div class="bg-dark d-flex align-items-center justify-content-center rounded mb-2" style="width: 60px; height: 60px;">
-                        <i class="fa fa-check text-white"></i>
-                    </div>
+                    <h1 class="mb-0" data-toggle="counter-up">14000</h1>
                     <div class="ps-4">
-                        <h1 class="mb-0" data-toggle="counter-up">14000</h1>
-                        <h5 class="text-dark mb-0">Empleos Generados</h5>
+                        <h3 class="text-dark mb-0">Empleos Generados</h3>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 wow zoomIn" data-wow-delay="0.6s">
                 <div class="bg-dark shadow d-flex align-items-center justify-content-center p-4" style="height: 150px;">
-                    <div class="bg-white d-flex align-items-center justify-content-center rounded mb-2" style="width: 60px; height: 60px;">
-                        <i class="fa fa-award text-dark"></i>
-                    </div>
+                    <h1 class="text-white mb-0" data-toggle="counter-up">180</h1>
                     <div class="ps-4">
-                        <h1 class="text-white mb-0" data-toggle="counter-up">180</h1>
-                        <h5 class="text-white mb-0">Actividades por año</h5>
+                        <h3 class="text-white mb-0">Actividades por año</h3>
                     </div>
                 </div>
             </div>
