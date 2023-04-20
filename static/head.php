9936feca85b7208cf4f6ac0b322d7980cf1aaa59
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2023-04-20 11:47:48 -0600

    Subiendo cambios

--- static/head.php
+++ static/head.php
@@ -5,8 +5,6 @@
 <meta content="Somos un sindicato patronal apartidista y de afiliación voluntaria que reúne a empresarios de todos los tamaños" name="description">
 <meta content="WhiteBox Systems" name="author">
 <link rel="manifest" href="manifest.json" />
-
-<!-- Favicon -->
 <link href="assets/img/LogoVertical.png" rel="icon">
 <link rel="apple-touch-icon" href="assets/img/icons/logo_72x72.png" />
 <link rel="apple-touch-icon" href="assets/img/icons/logo_96x96.png" />
@@ -16,24 +14,13 @@
 <link rel="apple-touch-icon" href="assets/img/icons/logo_192x192.png" />
 <link rel="apple-touch-icon" href="assets/img/icons/logo_384x384.png" />
 <link rel="apple-touch-icon" href="assets/img/icons/logo_512x512.png" />
-
-
-<!-- Google Web Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet">
-
-<!-- Icon Font Stylesheet -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
-<!-- Libraries Stylesheet -->
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet" media="all">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet" media="all">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" media="all">
 <link href="assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 <link href="assets/lib/animate/animate.min.css" rel="stylesheet">
-
-<!-- Customized Bootstrap Stylesheet -->
-<link href="assets/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Template Stylesheet -->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" media="all">
 <link href="assets/css/style.css" rel="stylesheet">
 <script src="assets/js/app.js"></script>
