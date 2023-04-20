04850766bf474f3606445cf56f4d615ace32e411
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2023-04-20 13:29:31 -0600

    Subiendo cambios

--- components/FactsStats.php
+++ components/FactsStats.php
@@ -7,7 +7,7 @@
                         <i class="fa fa-users" style="color:#000"></i>
                     </div>
                     <div class="ps-4">
-                        <h3 class="mb-0" style="color:#232F5D" >Empresas</h3>
+                        <h4 class="mb-0" style="color:#232F5D">Empresas</h4>
                         <div class="txtMas">
                             <h4 class="mb-0" style="color:#232F5D">+</h4>
                             <h4 class="mb-0" data-toggle="counter-up" style="color:#232F5D">180</h4>
@@ -16,15 +16,15 @@
                 </div>
             </div>
             <div class="col-lg-4 wow zoomIn" data-wow-delay="0.3s">
-                <div class="shadow d-flex align-items-center justify-content-center p-4"  style="height: 150px; background-color:#232F5D">
+                <div class="shadow d-flex align-items-center justify-content-center p-4" style="height: 150px; background-color:#232F5D">
                     <div class="d-flex align-items-center justify-content-center rounded mb-2" style="width: 60px; height: 60px;">
                         <i class="fa fa-check text-white"></i>
                     </div>
                     <div class="ps-4">
-                        <h3 class="mb-0" style="color:#fff">Empleos Generados</h3>
+                        <h4 class="mb-0" style="color:#fff">Empleos Generados</h4>
                         <div class="txtMas">
-                            <h4 class="mb-0"  style="color:#fff">+</h4>
-                            <h4 class="mb-0" data-toggle="counter-up"  style="color:#fff">14000</h4>
+                            <h4 class="mb-0" style="color:#fff">+</h4>
+                            <h4 class="mb-0" data-toggle="counter-up" style="color:#fff">14000</h4>
                         </div>
                     </div>
                 </div>
@@ -35,7 +35,7 @@
                         <i class="fa fa-users" style="color:#000"></i>
                     </div>
                     <div class="ps-4">
-                        <h3 class="mb-0" style="color:#232F5D" >Actividades por año</h3>
+                        <h4 class="mb-0" style="color:#232F5D">Actividades por año</h4>
                         <div class="txtMas">
                             <h4 class="mb-0" style="color:#232F5D">+</h4>
                             <h4 class="mb-0" data-toggle="counter-up" style="color:#232F5D">180</h4>
