3ea1b9383ca069ff6e4b6e4aee2b4cc3d1d2f798
Kevinmejia12 <kevinjaime0104@gmail.com>
2023-04-20 12:41:56 -0600

    links en el footer

--- static/footer.php
+++ static/footer.php
@@ -70,3 +70,8 @@
 <script src="assets/lib/counterup/counterup.min.js"></script>
 <script src="assets/lib/owlcarousel/owl.carousel.min.js"></script>
 <script src="assets/js/main.js"></script>
+
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
