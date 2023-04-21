59fc92dd6d2af78014db00d3006e52d93886b1d9
JuanPabloHack <juanpablodejesusfigueroa@gmail.com>
2023-04-20 20:01:42 -0600

    subiendo cambios

--- static/head.php
+++ static/head.php
@@ -21,6 +21,4 @@
 <link media="all" href="assets/css/bootstrap.min.css" rel="stylesheet">
 <link href="assets/css/style.css" rel="stylesheet">
 <link media="all" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet">
-<link media="all" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-<link media="all" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 <script src="assets/js/app.js"></script>
