d0844f58fb4180e4d6347e8a7db6253f40cd1d07
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2023-05-09 12:53:37 -0600

    Mejorando los blogs

--- components/GridBlog.php
+++ components/GridBlog.php
@@ -43,11 +43,9 @@
                         <h3 class="mb-0">Categories</h3>
                     </div>
                     <div class="link-animated d-flex flex-column justify-content-start">
-                        <a class="h5 fw-semi-bold bg-light rounded py-2 px-3 mb-2" href="#"><i class="bi bi-arrow-right me-2"></i>Fiscal</a>
-                        <a class="h5 fw-semi-bold bg-light rounded py-2 px-3 mb-2" href="#"><i class="bi bi-arrow-right me-2"></i>Laboral</a>
-                        <a class="h5 fw-semi-bold bg-light rounded py-2 px-3 mb-2" href="#"><i class="bi bi-arrow-right me-2"></i>Economía</a>
-                        <a class="h5 fw-semi-bold bg-light rounded py-2 px-3 mb-2" href="#"><i class="bi bi-arrow-right me-2"></i>Desarrollo Empresarial</a>
-                        <a class="h5 fw-semi-bold bg-light rounded py-2 px-3 mb-2" href="#"><i class="bi bi-arrow-right me-2"></i>Educación y Capacitación</a>
+                        <?php foreach (obtenerCategorias() as $row) { ?>
+                            <a class="h5 fw-semi-bold bg-light rounded py-2 px-3 mb-2" href="#"><i class="bi bi-arrow-right me-2"></i><?php echo $row['nombre'] ?></a>
+                        <?php } ?>
                     </div>
                 </div>
                 <!-- TODO:  aqui tendrías que mostrar solo los blogs mas recientes por lo que en la cosulta tendrás quie validar el campo de fecha ponerlas de forma ascendente, y solo tendrías que mostrar los primeros 5 resultados, de manera que con un ciclo while o un foreach puedas recorrer todos los resultados que te arroja la base de datos -->
@@ -56,11 +54,13 @@
                         <h3 class="mb-0">Post Recientes</h3>
                     </div>
                     <div class="d-flex rounded overflow-hidden mb-3">
-                        <!-- TODO: Aqui vas a mostrar una imagen que estará en la base de datos la cual la pondras en la variable del atributo src en este caso, en vez de estar "assets/img/blog-1.jpg", estaría algo tipo $mostrar_datos['ruta_imagen'] usando las propiedades de php con el objeto de mysql -->
-                        <img class="img-fluid" data-src="assets/img/blog-1.jpg" style="width: 100px; height: 100px; object-fit: cover;" alt="">
-                        <!-- TODO: Aqui vas a poner el titulo dentro de la etiqueta a con la variable de la categoria del blog en su caso sería algo tipo así $mostrar_datos['id_categoria'], sería igual para las demás etiquetas que siguen el titulo, el resumen, la fecha y el autor -->
-                        <a href="" class="h5 fw-semi-bold d-flex align-items-center bg-light px-3 mb-0">Lorem ipsum dolor sit amet adipis elit
-                        </a>
+                        <?php foreach (obtenerBlogsPublicados() as $row) { ?>
+                            <!-- TODO: Aqui vas a mostrar una imagen que estará en la base de datos la cual la pondras en la variable del atributo src en este caso, en vez de estar "assets/img/blog-1.jpg", estaría algo tipo $mostrar_datos['ruta_imagen'] usando las propiedades de php con el objeto de mysql -->
+                            <img class="img-fluid" data-src="assets/blog/<?php echo $row['titulo'] . '/' . $row['foto']  ?>" style="width: 100px; height: 100px; object-fit: cover;" alt="Recents Blog">
+                            <!-- TODO: Aqui vas a poner el titulo dentro de la etiqueta a con la variable de la categoria del blog en su caso sería algo tipo así $mostrar_datos['id_categoria'], sería igual para las demás etiquetas que siguen el titulo, el resumen, la fecha y el autor -->
+                            <a href="./detail.php?id_blog=<?php echo $row['id'] ?>" class="h5 fw-semi-bold d-flex align-items-center bg-light px-3 mb-0"><?php echo $row['titulo']  ?>
+                            </a>
+                        <?php } ?>
                     </div>
                 </div>
             </div>
