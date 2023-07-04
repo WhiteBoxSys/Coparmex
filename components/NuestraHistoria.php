64c998e9838febfbfaa4743b36c7c61928513340
JuanPabloHack <juanpablodejesusfigueroa@gmail.com>
2023-07-04 16:39:58 -0600

    Subiendo cambios

--- components/NuestraHistoria.php
+++ components/NuestraHistoria.php
@@ -5,14 +5,25 @@
                 <div class="section-title position-relative pb-3 mb-5">
                     <h1 class="mb-0">Nuestra Historia</h1>
                 </div>
-                <p class="mb-4" style="text-align: justify;">Fundado en el año de 1999, cuando un grupo de empresarios asentados en Manzanillo y por iniciativa de un par de amigos empresarios, Alejandro Dávila y Enrique Arévalo, buscando hacer una forma de cohesion empresarial, iniciaron con la idea de afiliar a las empresas de Manzanillo con el Centro Empresarial de Colima.</p>
+                <p class="mb-4" style="text-align: justify;">Fundado en el año de 1999, cuando un grupo de empresarios
+                    asentados en Manzanillo y por iniciativa de un par de amigos empresarios, Alejandro Dávila y Enrique
+                    Arévalo, buscando hacer una forma de cohesion empresarial, iniciaron con la idea de afiliar a las
+                    empresas de Manzanillo con el Centro Empresarial de Colima.</p>
                 <p class="mb-4" style="text-align: justify;">
-                    La sorpresa fue que 6 meses después de iniciado este proyecto, el numero de empresas de Manzanillo afiliadas seria lo suficientemente grande para ser un centro empresarial independiente, por lo que se logro conformar el Centro Empresarial de Manzanillo, logrando su afiliación a la Confederación Patronal de la Republica Mexicana, COPARMEX, el sindicato patronal de México Hoy gracias a la participación activa de todos los socio el trabajo constante de consejeros, consejos de expresidentes, el apoyo de los socios, staff y a todas las familias de los que nos acompañan en este proyecto, la representatividad que tienen las empresas que lo conforman, su numero de colaboradores y su aporte a la economía local, estatal y federal, todos en conjunto, se ha logrado que el centro patronal este fortalecido, unido y con mayor representatividad en el estado.
+                    La sorpresa fue que 6 meses después de iniciado este proyecto, el numero de empresas de Manzanillo
+                    afiliadas seria lo suficientemente grande para ser un centro empresarial independiente, por lo que
+                    se logro conformar el Centro Empresarial de Manzanillo, logrando su afiliación a la Confederación
+                    Patronal de la Republica Mexicana, COPARMEX, el sindicato patronal de México Hoy gracias a la
+                    participación activa de todos los socio el trabajo constante de consejeros, consejos de
+                    expresidentes, el apoyo de los socios, staff y a todas las familias de los que nos acompañan en este
+                    proyecto, la representatividad que tienen las empresas que lo conforman, su numero de colaboradores
+                    y su aporte a la economía local, estatal y federal, todos en conjunto, se ha logrado que el centro
+                    patronal este fortalecido, unido y con mayor representatividad en el estado.
                 </p>
             </div>
             <div class="col-lg-5" style="min-height: 500px;">
                 <div class="position-relative h-100">
-                    <img class="position-absolute w-100 h-100 rounded wow zoomIn" data-wow-delay="0.9s" data-src="assets/img/IA 3.2.png" style="object-fit: cover;" alt="NuestraHistoria_Image">
+                    <img class="position-absolute w-100  rounded wow" data-wow-delay="0.9s" data-src="assets/img/collage.jpg" style="object-fit: cover;" alt="NuestraHistoria_Image">
                 </div>
             </div>
         </div>
